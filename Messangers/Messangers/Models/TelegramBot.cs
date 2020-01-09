16f5dba9fbb62a2055b208ebcac10d9e5ba83e65
a.didenko <a.didenko@bpmonline.com>
2020-01-09 13:56:54 +0200

    Добавил класс для хранения ботов

---
+++ Messangers/Messangers/Models/TelegramBot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Messangers.Models
+{
+	public class TelegramBot
+	{
+		public int BotId {
+			get; set;
+		}
+
+		public string BotToken {
+			get; set;
+		}
+	}
+}
