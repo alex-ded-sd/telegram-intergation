d99e8b54eaabaaf2ad139b5122be2bcbb5fbe8eb
a.didenko.sd@gmail.com <a.didenko.sd@gmail.com>
2020-01-22 09:50:51 +0200

    Refactor Startup.cs

--- Messengers/Messengers/Startup.cs
+++ Messengers/Messengers/Startup.cs
@@ -6,39 +6,35 @@ namespace Messengers
 	using Microsoft.Extensions.Configuration;
 	using Microsoft.Extensions.DependencyInjection;
 	using Microsoft.Extensions.Hosting;
-	using MongoDB.Bson.Serialization;
-	using Telegram.Bot;
 
 	public class Startup
 	{
-		public Startup(IConfiguration configuration) {
+		public Startup(IConfiguration configuration)
+		{
 			Configuration = configuration;
 		}
 
-		public IConfiguration Configuration {
+		public IConfiguration Configuration
+		{
 			get;
 		}
 
 		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services) {
+		public void ConfigureServices(IServiceCollection services)
+		{
 			services.Configure<DbStoreSettings>(Configuration.GetSection(nameof(DbStoreSettings)));
 			services.AddScoped<TlgBotStorage>();
 			services.AddScoped<TelegramBotHandler>();
-			services.AddCors(options =>
-			{
-				options.AddPolicy("CorsPolicy", builder => builder
-					.WithOrigins("http://localhost:4200")
-					.AllowAnyMethod()
-					.AllowAnyHeader()
-					.AllowCredentials());
-			});
+			services.AddCors();
 			services.AddSignalR();
 			services.AddControllers();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-			if (env.IsDevelopment()) {
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
 				app.UseDeveloperExceptionPage();
 			}
 
@@ -46,10 +42,11 @@ namespace Messengers
 
 			app.UseRouting();
 
-			app.UseCors("CorsPolicy");
+			app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 
-			app.UseEndpoints(endpoints => {
+			app.UseEndpoints(endpoints =>
+			{
 				endpoints.MapHub<ChatHub>("/chathub");
 				endpoints.MapControllers();
 			});
